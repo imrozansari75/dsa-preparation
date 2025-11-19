5fef6340814f3f10ca81b593d2fdedabf939cc76
Imroz Ansari <mr.imroz.ansari@gmail.com>
2025-11-19 18:51:59 +0530

    Optimum solution

--- array/15. 3sum/loop.js
+++ array/15. 3sum/loop.js
@@ -1,18 +1,19 @@
-nums = [1,0,-1,-5,8,4,-2]
-target = 6
-n=nums.length
-nums.sort((a,b) => a-b)
-result = []
+let nums = [1, 0, -1, -5, 8, 4, -2];
+let target = 6;
 
-for(i=0;i<n;i++){
-    for(j=i;j<n;j++){
-        for(k=j;k<n;k++){
-            sum = nums[i] + nums[j] + nums[k]
-            if(sum===target){
-                result.push([nums[i], nums[j], nums[k]])
+nums.sort((a, b) => a - b);
+let result = [];
+let n = nums.length;
+
+for (let i = 0; i < n - 2; i++) {
+    for (let j = i + 1; j < n - 1; j++) {
+        for (let k = j + 1; k < n; k++) {
+            let sum = nums[i] + nums[j] + nums[k];
+            if (sum === target) {
+                result.push([nums[i], nums[j], nums[k]]);
             }
         }
-    }    
+    }
 }
 
-console.log(result)
+console.log(result);
