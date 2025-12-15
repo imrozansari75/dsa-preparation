07ca67e6df591c7765dcc8fdcef7e1076f6e249d
Imroz Ansari <mr.imroz.ansari@gmail.com>
2025-12-15 21:45:20 +0530

    Append on specific index

--- Linked List/LinkedList_2.js
+++ Linked List/LinkedList_2.js
@@ -10,6 +10,7 @@ class LinkedList {
     this.head = null;
   }
 
+  // Inserts a new node with the given data at the end of the list
   append(data) {
     const newNode = new Node(data);
 
@@ -25,11 +26,43 @@ class LinkedList {
     current.next = newNode;
   }
 
+  // Inserts a new node with the given data at the beginning of the list
   insertAtHead(data) {
     const newNode = new Node(data);
     newNode.next = this.head;
     this.head = newNode;
   }
+
+  // Inserts a new node with the given data at the specified index
+  insertAtIndex(data, index) {
+  // Case 1: insert at head
+  if (index === 0) {
+    const newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+    return;
+  }
+
+  const newNode = new Node(data);
+  let current = this.head;
+  let count = 0;
+
+  // Move to (index - 1) node
+  while (current && count < index - 1) {
+    current = current.next;
+    count++;
+  }
+
+  if (!current) {
+    console.log("Index out of bounds");
+    return;
+  }
+
+  // Insert node
+  newNode.next = current.next;
+  current.next = newNode;
+}
+
 }
 
 const list = new LinkedList();
@@ -39,5 +72,7 @@ list.append(30);
 
 list.insertAtHead(5);
 list.insertAtHead(0);
+
+list.insertAtIndex(15, 2); // Insert 15 at index 2
 
 console.log(list.head);
