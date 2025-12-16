93bac2fec63f99850bd2656ffd15aa8ba449ee5e
Imroz Ansari <mr.imroz.ansari@gmail.com>
2025-12-16 17:01:17 +0530

    delete head of Linked List

---
+++ Linked List/LinkedList_3.js
@@ -0,0 +1,69 @@
+// Node class
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+// Linked List class
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // Insert at end
+  append(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
+
+  // Delete head
+  deleteHead() {
+    if (!this.head) {
+      console.log("List is empty");
+      return;
+    }
+    this.head = this.head.next;
+  }
+
+  // Print list
+  print() {
+    let current = this.head;
+    let result = "";
+
+    while (current) {
+      result += current.data + " → ";
+      current = current.next;
+    }
+
+    console.log(result + "null");
+  }
+}
+
+// ================== USAGE ==================
+
+const list = new LinkedList();
+
+list.append(10);
+list.append(20);
+list.append(30);
+
+console.log("Before deleting head:");
+list.print();
+
+list.deleteHead();
+
+console.log("After deleting head:");
+list.print();
