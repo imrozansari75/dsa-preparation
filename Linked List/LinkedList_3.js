83f093356013c717b687375f4dfb1db122647eb9
Imroz Ansari <mr.imroz.ansari@gmail.com>
2025-12-16 17:10:48 +0530

    Delete the end node

--- Linked List/LinkedList_3.js
+++ Linked List/LinkedList_3.js
@@ -29,13 +29,37 @@ class LinkedList {
     current.next = newNode;
   }
 
-  // Delete head
+  // ❌ Delete head
   deleteHead() {
     if (!this.head) {
       console.log("List is empty");
       return;
     }
+
     this.head = this.head.next;
+  }
+
+  // ❌ Delete end
+  deleteEnd() {
+    if (!this.head) {
+      console.log("List is empty");
+      return;
+    }
+
+    // If only one node
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let current = this.head;
+
+    // Stop at second-last node
+    while (current.next.next) {
+      current = current.next;
+    }
+
+    current.next = null;
   }
 
   // Print list
@@ -59,11 +83,15 @@ const list = new LinkedList();
 list.append(10);
 list.append(20);
 list.append(30);
+list.append(40);
 
-console.log("Before deleting head:");
+console.log("Initial list:");
 list.print();
 
 list.deleteHead();
-
 console.log("After deleting head:");
+list.print();
+
+list.deleteEnd();
+console.log("After deleting end:");
 list.print();
